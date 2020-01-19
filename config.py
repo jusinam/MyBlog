5022fced59e3b3c181ee4c3957ffd796a631c104
DjCooGie <evansonchagwa01@gmail.com>
2020-01-19 14:16:09 +0300

    import the OS module and add app's configuratons

---
+++ config.py
@@ -0,0 +1,26 @@
+import os 
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:hahaa@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos' 
+
+    
+    
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    '''
+    DEBUG = True
+    
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
