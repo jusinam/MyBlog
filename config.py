183850f72afa08b71d4ad929b593d509c4c3f077
DjCooGie <evansonchagwa01@gmail.com>
2020-01-20 16:24:10 +0300

    heroku deployment

--- config.py
+++ config.py
@@ -1,26 +1,34 @@
-import os 
+import os
 
 class Config:
+    '''
+    Parent configuration class
+    '''
+    debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:hahaa@localhost/blog'
-    UPLOADED_PHOTOS_DEST ='app/static/photos' 
-
-    
-    
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
     '''
     Production configuration child class
     '''
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     '''
     Development configuration child class
     '''
+    SECRET_KEY = 'hahaha'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:hahaa@localhost/blog'
     DEBUG = True
-    
+    ENV = 'development'
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig
+    'development':DevConfig,
+    'production':ProdConfig,
 }
--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from  flask_migrate import Migrate, MigrateCommand
-from app.models import User
+from app.models import User, Blog, Comment
 
-#Creating app instance
-app = create_app('development')
+# Creating app instance
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
@@ -20,7 +20,7 @@ def test():
     
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User, Blog = Blog, Comment = Comment)
 
 if __name__ == "__main__":
     manager.run()
