c40d78fd9d8ba6ebffe8b44c526d77a731cec579
DjCooGie <evansonchagwa01@gmail.com>
2020-01-20 11:00:22 +0300

    create Blog and Comment model classes

--- app/models.py
+++ app/models.py
@@ -8,32 +8,30 @@ from . import login_manager
 
 
 
-
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    bio  = db.Column(db.String(255),default='My Bio')
-    profile_pic_path = db.Column(db.String(255),default='blogger.png')
-    pass_secure = db.Column(db.String(255))
-  
-  
-
+class User(db.Model,UserMixin):
     
+    __tablename__ = 'users'
+
+    id =db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(255),unique=True,nullable=False)
+    email = db.Column(db.String(255),unique=True,nullable=False)
+    bio  = db.Column(db.String(255),default='My Bio')
+    profile_pic_path = db.Column(db.String(255),default='blog.png')
+    password_hash = db.Column(db.String(255),nullable=False)
+    blog = db.relationship('Blog',backref='user',lazy='dynamic')
+    comment = db.relationship('Comment',backref='user',lazy='dynamic')
+
     @property
-    def password(self):
+    def set_password(self):
         raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
-
+    @set_password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
-    
+        return check_password_hash(self.password_hash,password)
+
     def save_user(self):
         db.session.add(self)
         db.session.commit()
@@ -43,21 +41,60 @@ class User(db.Model, UserMixin):
         db.session.commit()
 
     def __repr__(self):
-        return f'User {self.username}'
-    
-    
-class Role(db.Model):
-    __tablename__ = 'roles'
+        return "User: %s " %str(self.username)
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
 
     id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    title =  db.Column(db.String(255),nullable = False)
+    content = db.Column(db.Text(),nullable = False)
+    posted = db.Column(db.DateTime,default = datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship('Comment',backref='blog',lazy='dynamic')
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    
+    def get_blog(self,id):
+        blog = Blog.query.filter_by(id=id).first()
+        return blog
 
     def __repr__(self):
-        return f'User {self.name}'
+        return f'Blog {self.title}'
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,primary_key =True)
+    comment = db.Column(db.String(255))
+    posted = db.Column(db.DateTime,default = datetime.utcnow)
+    blog_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
 
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
 
+    def delete_commit(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # @classmethod
+    def get_comment(self,id):
+        comment = Comment.query.all(id=id)
+        return comment
+
+
+    def __repr__(self):
+        return f'Comment {self.comment}'
 
 
 @login_manager.user_loader
